07f1f57b9aad867743de737e55591843c4b97437
michaeltimmler91-jpg <michaeltimmler91@googlemail.com>
2026-05-16 07:15:41 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const SUPABASE_URL = "https://unkfqoplynwabulnzpar.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_AkIVrLBsgIV2jYJ5gGsBmw_f7P62KTK";
 
-const client = supabase.createClient(
+window.client = supabase.createClient(
     SUPABASE_URL,
     SUPABASE_ANON_KEY
 );
