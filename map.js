2ba9d7e01a9f3edbc608b9d89f5b861be157bc1d
michaeltimmler91-jpg <michaeltimmler91@googlemail.com>
2026-05-16 08:34:43 +0200

    Update map.js

--- map.js
+++ map.js
@@ -1,29 +1,3 @@
-const PLZ_MAP = {
-    "8054": {
-        name: "Mirror Park",
-        x: 58,
-        y: 42
-    },
-
-    "8170": {
-        name: "LSMD",
-        x: 49,
-        y: 63
-    },
-
-    "10012": {
-        name: "Hookies",
-        x: 54,
-        y: 67
-    },
-
-    "9321": {
-        name: "Sandy Shores",
-        x: 55,
-        y: 30
-    }
-};
-
 function searchPlz() {
     const input = document.getElementById("plz_search").value.trim();
     const result = document.getElementById("plz_result");
