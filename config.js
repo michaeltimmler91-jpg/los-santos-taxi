656c435bebf53c046dde68251489f36ae242bf8c
michaeltimmler91-jpg <michaeltimmler91@googlemail.com>
2026-05-17 18:10:04 +0200

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-const SUPABASE_URL = "https://unkfqoplynwabulnzpar.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_AkIVrLBsgIV2jYJ5gGsBmw_f7P62KTK";
-
-window.client = supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_ANON_KEY
-);
