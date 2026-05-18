dc6745918945d2d962e4416bc92f120f2cbec77a
michaeltimmler91-jpg <michaeltimmler91@googlemail.com>
2026-05-18 10:45:38 +0200

    Create config.js

---
+++ homepage/config.js
@@ -0,0 +1,11 @@
+const SUPABASE_URL =
+"https://unkfqoplynwabulnzpar.supabase.co";
+
+const SUPABASE_KEY =
+"sb_publishable_AkIVrLBsgIV2jYJ5gGsBmw_f7P62KTK";
+
+const supabaseClient =
+supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
